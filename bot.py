2d9b8cb499d9edae72b298e2787589fe03235221
austinkelsay <austinkelsay@yahoo.com>
2022-12-07 14:14:42 -0600

    Moving send_message inside of run_discord_bot scope

--- bot.py
+++ bot.py
@@ -2,19 +2,6 @@ import discord
 import responses
 import os
 
-
-async def send_message(message, user_message, is_private):
-    # Get the response for the user's message
-    response = responses.handle_response(user_message)
-
-    try:
-        # Send the response to the user
-        await message.author.send(response)
-    except Exception as e:
-        # Print any errors that occur
-        print("my exception", e)
-        await message.channel.send("Sorry I timed out trying to send you a message. Please try again.")
-
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -33,6 +20,18 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
+
+    async def send_message(message, user_message, is_private):
+        # Get the response for the user's message
+        response = responses.handle_response(user_message)
+
+        try:
+            # Send the response to the user
+            await message.author.send(response)
+        except Exception as e:
+            # Print any errors that occur
+            print("my exception", e)
+            await message.channel.send("Sorry I timed out trying to send you a message. Please try again.")
 
     # Remember to run your bot with your personal TOKEN
     client.run(TOKEN)
