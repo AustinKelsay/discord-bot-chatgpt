e587260d57c8ae2dce257a34cfc77e47d7f87840
austinkelsay <austinkelsay@yahoo.com>
2022-12-07 15:22:22 -0600

    Using command now

--- bot.py
+++ bot.py
@@ -1,37 +1,52 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import responses
 import os
 
+
+async def send_message(message, user_message):
+    try:
+        response = '> **' + user_message + '** - <@' + \
+            str(message.user.id) + '>\n\n' + \
+            responses.handle_response(user_message)
+        if len(response) > 1900:
+            # Split the response into smaller chunks of no more than 1900 characters each(Discord limit is 2000 per chunk)
+            response_chunks = [response[i:i+1900]
+                               for i in range(0, len(response), 1900)]
+            for chunk in response_chunks:
+                await message.followup.send(chunk)
+        else:
+            await message.followup.send(response)
+    except Exception as e:
+        await message.followup.send("> **Error: There are something went wrong. Please try again later!**")
+        print(e)
+
 intents = discord.Intents.default()
 intents.message_content = True
-
-
-client = discord.Client(intents=intents)
+is_private = True
 
 
 def run_discord_bot():
-    # Change your token here
-    global client
-    if client == None:
-        client = discord.Client(intents=intents)
-
     TOKEN = os.getenv("DISCORD_TOKEN")
+
+    client = commands.Bot(command_prefix='!', intents=intents)
 
     @client.event
     async def on_ready():
+        await client.tree.sync()
         print(f'{client.user} is now running!')
 
-    # Listen for a dm
-    @client.event
-    async def on_message(message):
-        response = responses.handle_response(message.content)
-
-        try:
-            # Send the response to the user
-            await message.author.send(response)
-        except Exception as e:
-            # Print any errors that occur
-            print("my exception", e)
-            await message.channel.send("Sorry I timed out trying to send you a message. Please try again.")
+    @client.tree.command(name="chat", description="Have a chat with chatGPT")
+    async def chat(interaction: discord.Interaction, *, message: str):
+        print('Chat command received', message)
+        if interaction.user == client.user:
+            return
+        username = str(interaction.user)
+        user_message = message
+        channel = str(interaction.channel)
+        print(f"{username} said: '{user_message}' ({channel})")
+        await interaction.response.defer(ephemeral=is_private)
+        await send_message(interaction, user_message)
 
     client.run(TOKEN)
