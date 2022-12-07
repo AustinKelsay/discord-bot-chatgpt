cc06f8efcf6cb98f0a5bf7b6845740281316f95c
austinkelsay <austinkelsay@yahoo.com>
2022-12-06 18:23:14 -0600

    Getting rid of dotenv import

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import discord
 import responses
 import os
-import dotenv
-
-dotenv.load_dotenv()
 
 # Send messages
+
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message)
@@ -28,35 +27,37 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 def run_discord_bot():
     # Change your token here
     global client
     if client == None:
-      client = discord.Client(intents=intents)
+        client = discord.Client(intents=intents)
 
     TOKEN = os.getenv("DISCORD_TOKEN")
+
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
 
     @client.event
     async def on_message(message):
-      if message.author == client.user:
-          return
-  
-      # Check if the message was sent in a DM
-      if message.channel.type == discord.ChannelType.private:
-          username = str(message.author)
-          user_message = str(message.content)
-  
-          with open("questions.txt", "a") as f:
-              f.write(user_message + "\n")
-  
-          if user_message[0] == '?':
-              user_message = user_message[1:]
-              await send_message(message, user_message, is_private=True)
-          else:
-              await send_message(message, user_message, is_private=False)
+        if message.author == client.user:
+            return
+
+        # Check if the message was sent in a DM
+        if message.channel.type == discord.ChannelType.private:
+            username = str(message.author)
+            user_message = str(message.content)
+
+            with open("questions.txt", "a") as f:
+                f.write(user_message + "\n")
+
+            if user_message[0] == '?':
+                user_message = user_message[1:]
+                await send_message(message, user_message, is_private=True)
+            else:
+                await send_message(message, user_message, is_private=False)
 
     # Remember to run your bot with your personal TOKEN
     client.run(TOKEN)
